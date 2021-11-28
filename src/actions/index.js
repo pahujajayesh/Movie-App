d4654d963a0a5095df21a5d609ce5b9d95d48424
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-11-28 20:33:31 +0530

    Changing state to add favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
-// {
-//     type:'ADD_MOVIES'
-// }
+//action types
+
+export const ADD_MOVIES='ADD_MOVIES';
+
+//action creators
+export function addMovies(movies){
+    return{
+    type:ADD_MOVIES,
+    movies
+    }
+}
