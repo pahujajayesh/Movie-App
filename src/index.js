d8eae87ddf76835fc4c4fdf7872fb6c65eb4437c
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-11-28 16:04:01 +0530

    Add Redux and store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './Components/App';
+import movies from './reducers';
+
+const store=createStore(movies);
+console.log('Store',store);
+console.log('Store',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
