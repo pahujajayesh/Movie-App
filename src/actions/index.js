8c9ccfbc5f5966ddc57a6f75e98a645e62bffda9
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-12-03 19:54:58 +0530

    add change tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@
 export const ADD_TO_FAVOURITES='ADD_TO_FAVOURITES'
 export const REMOVE_FROM_FAVOURITES='REMOVE_FROM_FAVOURITES';
 export const ADD_MOVIES='ADD_MOVIES';
+export const SET_SHOW_FAVOURITES='SET_SHOW_FAVOURITES';
 
 //action creators
 export function addMovies(movies){
@@ -22,4 +23,11 @@ export function removeFromFavourites(movie){
         type:REMOVE_FROM_FAVOURITES,
         movie
     }
+}
+export function setShowFavourites(val){
+    return{
+        type:SET_SHOW_FAVOURITES,
+        val
+    }
+
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITES,REMOVE_FROM_FAVOURITES } from "../actions";
+import { ADD_MOVIES, ADD_TO_FAVOURITES,REMOVE_FROM_FAVOURITES,SET_SHOW_FAVOURITES } from "../actions";
 const initialMoviesState = {
   list: [],
   favourites: [],
+  showFavourites:false
 };
 export default function movies(state = initialMoviesState, action) {
   switch (action.type) {
@@ -24,6 +25,12 @@ export default function movies(state = initialMoviesState, action) {
       return{
         ...state,
         favourites:filteredArray
+      }
+    }
+    case SET_SHOW_FAVOURITES:{
+      return{
+        ...state,
+        showFavourites:action.val
       }
     }
     default:
