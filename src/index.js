5acccf4aff2c434f13e9efbabd978df1ddf2fac6
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-12-05 09:29:37 +0530

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './Components/App';
 import rootReducer from './reducers';
-
-const store=createStore(rootReducer);
+const logger=function({dispatch,getState}){
+  return function(next){
+    return function(action){
+      console.log('ACTION_TYPE:', action.type);
+      next(action);
+    }
+  }
+}
+const store=createStore(rootReducer,applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
