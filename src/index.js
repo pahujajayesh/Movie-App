847a1c46dda7435f6680044bdc69acdbe8f0fefa
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-12-05 09:35:10 +0530

    Modified middleware

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import { createStore,applyMiddleware } from 'redux';
 import './index.css';
 import App from './Components/App';
 import rootReducer from './reducers';
-const logger=function({dispatch,getState}){
-  return function(next){
-    return function(action){
-      console.log('ACTION_TYPE:', action.type);
-      next(action);
-    }
-  }
+// const logger=function({}dispatch,getState){
+//   return function(next){
+//     return function(action){
+//       console.log('ACTION_TYPE:', action.type);
+//       next(action);
+//     }
+//   }
+// }
+const logger=({dispatch,getState})=>(next)=>(action)=>{
+  console.log('ACTION_TYPE:', action.type);
+  next(action);
 }
 const store=createStore(rootReducer,applyMiddleware(logger));
 
